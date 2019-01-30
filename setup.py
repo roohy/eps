726242e7c83b901393ab2f55cf84ce1bfd26c666
Ruhollah Shemirani <Roohy@roohy.isi.edu>
2019-01-29 18:36:02 -0800

    package template

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pseudo-sampler-roohy",
+    version="0.1.1",
+    author="Ruhollah Shemirani",
+    author_email="shemirani.r@gmail.com",
+    description="Extreme Pseudo Sampling Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
